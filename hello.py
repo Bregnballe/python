277ddcb47a6d5f9eb8ba15491576496195123275
Bregnballe <bregnballe@gmail.com>
2023-05-10 15:39:33 +0200

    Changed the organisation of the json and added some commments

--- hello.py
+++ hello.py
@@ -3,11 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 import json
-import time
 import csv
-import pandas as pd
 
 url = "https://www.dst.dk/da/Statistik/emner/befolkning-og-valg/navne/HvorMange"
 PATH = "C:\Program Files (x86)\ChromeDriver\chromedriver.exe"
@@ -30,9 +27,9 @@ cookieButton.click()
 with open("C:/codetemp/python/names.csv", "r", encoding="UTF-8") as csv_read_file:
     csv_reader = csv.reader(csv_read_file)
 
-    for line in csv_reader:  # for every line in the csv files
-        searchInput.send_keys(line)  # add line to input and search
-        searchButton.click()
+    for name in csv_reader:  # for every line in the csv files
+        searchInput.send_keys(name)  # add line to input and search
+        searchButton.click()  # click the search button
 
         try:
             table = WebDriverWait(driver, 10).until(
@@ -40,21 +37,43 @@ with open("C:/codetemp/python/names.csv", "r", encoding="UTF-8") as csv_read_fil
             )  # wait until table is available
 
             tableData = [
-                [cell.text for cell in row.find_elements_by_tag_name("td")]
+                [
+                    "female"
+                    if cell.text.startswith("Kvinder")
+                    else "male"
+                    if cell.text.startswith("Mænd")
+                    else ""
+                    if cell.text.startswith("Der")
+                    else cell.text
+                    for cell in row.find_elements_by_tag_name("td")
+                ]
                 for row in table.find_elements_by_tag_name("tr")
             ][1:]
+            # The [1:] removes the first row (tr) which is the header row.
 
             tableHeader = [
-                [cell.text for cell in row.find_elements_by_tag_name("th")]
+                [
+                    "gender"
+                    if cell.text.startswith("Resultat")
+                    else "trend"
+                    if cell.text.startswith("Ændring")
+                    else cell.text
+                    for cell in row.find_elements_by_tag_name("th")
+                ]
                 for row in table.find_elements_by_tag_name("tr")
             ][0]
+            # The [0] selects only the first row (tr) which is the header row.
 
             zippedList = [dict(zip(tableHeader, t)) for t in tableData]
+            # zipping the two lists together and turn them into a dictionary (like an object)
 
-            resultList.append(zippedList)
+            data = {name[0]: zippedList}
+
+            resultList.append(data)
+            # Create and append an object with the name as key and value of zippedList
 
             clearButton.click()
 
-        except:
-            print("Something went wrong")
+        except Exception as e:
+            print(f"Something went wrong: {e}")
 print(json.dumps(resultList, indent=4, ensure_ascii=False))
